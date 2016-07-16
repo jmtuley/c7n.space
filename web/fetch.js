06ad0966938d7ebc57c20f6df1e2734b39912ed3
John Tuley <john@tuley.org>
2016-07-16 09:01:49 -0600

    Add static HTML and JS

---
+++ web/fetch.js
@@ -0,0 +1,18 @@
+$button = $('#fetch');
+
+$button.on('click', function() {
+    val = $('#contraction').val();
+
+    $.ajax({
+        url: 'en/' + val + '.json',
+        type: 'GET',
+        dataType: 'json',
+    }).done(function(json) {
+        list = '<ul>' + $.map(json.words, function(w) { return '<li>' + w + '</li>' }).join('\n') + '</ul>';
+        $('#results').html(list);
+    }).fail(function(xhr, status, error) {
+        console.log(xhr, status, error);
+    });
+
+    return false;
+});
