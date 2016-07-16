df0976b9f365f02f53f8159288e50b6b014a49ab
John Tuley <john@tuley.org>
2016-07-16 16:10:08 -0600

    Indent with two spaces instead of four

--- web/fetch.js
+++ web/fetch.js
@@ -1,18 +1,18 @@
 $button = $('#fetch');
 
 $button.on('click', function() {
-    val = $('#contraction').val().toLowerCase();
+  val = $('#contraction').val().toLowerCase();
 
-    $.ajax({
-        url: 'en/' + val + '.json',
-        type: 'GET',
-        dataType: 'json',
-    }).done(function(json) {
-        list = '<ul>' + $.map(json.words, function(w) { return '<li>' + w + '</li>' }).join('\n') + '</ul>';
-        $('#results').html(list);
-    }).fail(function(xhr, status, error) {
-        console.log(xhr, status, error);
-    });
+  $.ajax({
+    url: 'en/' + val + '.json',
+    type: 'GET',
+    dataType: 'json',
+  }).done(function(json) {
+    list = '<ul>' + $.map(json.words, function(w) { return '<li>' + w + '</li>' }).join('\n') + '</ul>';
+    $('#results').html(list);
+  }).fail(function(xhr, status, error) {
+    console.log(xhr, status, error);
+  });
 
-    return false;
+  return false;
 });
