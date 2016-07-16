273ea3d9e7f6825d8a3ee72b7a3a049e1eef795c
John Tuley <john@tuley.org>
2016-07-16 17:08:43 -0600

    Slim down copy, add permalinks and make more mobile-friendly

--- web/fetch.js
+++ web/fetch.js
@@ -3,16 +3,37 @@ $button = $('#fetch');
 $button.on('click', function() {
   val = $('#contraction').val().toLowerCase();
 
-  $.ajax({
-    url: 'en/' + val + '.json',
-    type: 'GET',
-    dataType: 'json',
-  }).done(function(json) {
-    list = '<ul>' + $.map(json.words, function(w) { return '<li>' + w + '</li>' }).join('\n') + '</ul>';
-    $('#results').html(list);
-  }).fail(function(xhr, status, error) {
-    console.log(xhr, status, error);
-  });
+  fetch(val);
 
   return false;
 });
+
+function fetch(contraction) {
+  $.ajax({
+    url: 'en/' + contraction + '.json',
+    type: 'GET',
+    dataType: 'json',
+  }).done(function(json) {
+    link = "<p>Share a <a href=" + baseURL() + "?c=en/" + contraction + ">link to these results.</a></p>\n";
+    list = '<ul>' + $.map(json.words, function(w) { return '<li>' + w + '</li>' }).join('\n') + '</ul>';
+    $('#results').html(link + list);
+  }).fail(function(xhr, status, error) {
+    console.log(xhr, status, error);
+  });
+}
+
+function baseURL() {
+  return window.location.href.split("?")[0];
+}
+
+$(function() {
+  wordParam = /\?c=(\w+)\/([A-Za-z]\d+[A-Za-z])$/;
+  match = window.location.href.match(wordParam);
+  if (match) {
+    _lang = match[1];
+    contraction = match[2];
+
+    fetch(contraction);
+    $('#contraction').val(contraction);
+  }
+})
